e9a54498e9c8a900ec926c0bb8588c361a073a0b
oban7 <fouchyelias@gmail.com>
2026-07-14 11:23:03 +0200

    Delete docadv 1.1 directory

--- docadv 1.1/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-/* =========================================================
-   Configuration publique du site.
-   Renseigner l'URL Google Apps Script après publication du Web App.
-   ========================================================= */
-window.ADVENTURE_DOG_CONFIG = {
-  googleAppsScriptUrl: '',
-  contactEmail: 'contact@adventure-dog-serrechevalier.fr',
-  contactPhone: '+33000000000'
-};
